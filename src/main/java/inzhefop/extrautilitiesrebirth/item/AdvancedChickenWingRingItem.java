a79fcd119ed28da0645f40dbad2ed394291bffc2
phoenix1leon@gmail.com <76850445+inzheFoP@users.noreply.github.com>
2022-10-16 14:05:51 +0200

    #advanced chicken ring disables fall damage

--- src/main/java/inzhefop/extrautilitiesrebirth/item/AdvancedChickenWingRingItem.java
+++ src/main/java/inzhefop/extrautilitiesrebirth/item/AdvancedChickenWingRingItem.java
@@ -1,13 +1,26 @@
 
 package inzhefop.extrautilitiesrebirth.item;
 
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
 
 import inzhefop.extrautilitiesrebirth.init.ExtrautilitiesrebirthModTabs;
 
 public class AdvancedChickenWingRingItem extends Item {
 	public AdvancedChickenWingRingItem() {
 		super(new Item.Properties().tab(ExtrautilitiesrebirthModTabs.TAB_TAB).durability(256).rarity(Rarity.UNCOMMON));
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(new TextComponent("\u00A77No fall-damage"));
 	}
 }
---
+++ src/main/java/inzhefop/extrautilitiesrebirth/procedures/PlayerFallsProcedure.java
@@ -0,0 +1,38 @@
+package inzhefop.extrautilitiesrebirth.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+
+import javax.annotation.Nullable;
+
+import inzhefop.extrautilitiesrebirth.init.ExtrautilitiesrebirthModItems;
+
+@Mod.EventBusSubscriber
+public class PlayerFallsProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player);
+		}
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof Player _playerHasItem
+				? _playerHasItem.getInventory().contains(new ItemStack(ExtrautilitiesrebirthModItems.ADVANCED_CHICKEN_WING_RING.get()))
+				: false) {
+			entity.fallDistance = 0;
+		}
+	}
+}
